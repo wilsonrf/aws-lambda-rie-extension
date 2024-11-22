cfff569470eec164813555cd0e41ce436c0595eb
Wilson da Rocha Franca <wilsonrf@gmail.com>
2024-11-22 11:38:42 +0100

    compile to bin/

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,10 @@ set -euo pipefail
 
 GOMOD=$(head -1 go.mod | awk '{print $2}')
 GOOS="linux" GOARCH="amd64" go build -ldflags='-s -w' -o linux/amd64/bin/main "$GOMOD/cmd/main"
+GOOS="linux" GOARCH="amd64" go build -ldflags='-s -w' -o bin/main "$GOMOD/cmd/main"
 
 ln -fs main linux/amd64/bin/generate
 ln -fs main linux/amd64/bin/detect
+
+ln -fs main bin/generate
+ln -fs main bin/detect
