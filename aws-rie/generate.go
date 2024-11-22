c6f8471ce520d178e2566e0233184f76b58a3a36
Wilson da Rocha Franca <wilsonrf@gmail.com>
2024-11-22 10:59:25 +0100

    implement generate

--- aws-rie/generate.go
+++ aws-rie/generate.go
@@ -1,6 +1,8 @@
 package awsrie
 
 import (
+	"strings"
+
 	"github.com/paketo-buildpacks/packit/v2"
 	"github.com/paketo-buildpacks/packit/v2/scribe"
 )
@@ -9,6 +11,19 @@ type Generate struct {
 	Logger scribe.Logger
 }
 
-func (b *Generate) Generate(packit.GenerateContext) (packit.GenerateResult, error) {
-	return packit.GenerateResult{}, nil
+func (b *Generate) Generate(context packit.GenerateContext) (packit.GenerateResult, error) {
+
+	r := strings.NewReader(`ARG base_image
+	FROM ${base_image}
+	RUN mkdir -p ~/.aws-lambda-rie
+	USER root
+	RUN apt-get update && apt-get install -y curl
+	RUN curl -Lo ~/.aws-lambda-rie/aws-lambda-rie https://github.com/aws/aws-lambda-runtime-interface-emulator/releases/latest/download/aws-lambda-rie
+	RUN chmod +x ~/.aws-lambda-rie/aws-lambda-rie
+	ARG user_id
+	USER ${user_id}`)
+
+	return packit.GenerateResult{
+		RunDockerfile: r,
+	}, nil
 }
